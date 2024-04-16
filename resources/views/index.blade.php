4b7eb0f14e08920f2697c30171b2a04523e94c99
echotamondong <shinoecho@gmail.com>
2024-04-16 19:42:53 +0800

    added final touches in table design css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,26 +2,26 @@
 <body class="bg-red-900">
 @include('partials.header')
     <div class="overflow-x-auto">
-        <table class="shadow-lg bg-white border-red-200">
+        <table class="shadow-lg bg-white">
             <thead>
-                <tr class="text-white bg-red-900 font-bold">
-                    <th class="px-6 py-3">ID</th>
-                    <th class="px-6 py-3">ISBN</th>
-                    <th class="px-6 py-3">Title</th>
-                    <th class="px-6 py-3">Author</th>
-                    <th class="px-6 py-3">Description</th>
-                    <th class="px-6 py-3">Date Published</th>
+                <tr class="text-white bg-red-900 font-bold border border-red-200">
+                    <th class="px-6 py-3 border border-red-200">ID</th>
+                    <th class="px-6 py-3 border border-red-200" >ISBN</th>
+                    <th class="px-6 py-3 border border-red-200">Title</th>
+                    <th class="px-6 py-3 border border-red-200">Author</th>
+                    <th class="px-6 py-3 border border-red-200">Description</th>
+                    <th class="px-6 py-3 border border-red-200">Date Published</th>
                 </tr>
             </thead>
-            <tbody class="text-red-900 text-left">
+            <tbody class="text-red-900 text-left border-1 border border-red-200">
                 @foreach($books as $book)
-                <tr class="bg-red-250">
-                    <td class="px-6 py-4">{{ $book->id }}</td>
-                    <td class="px-6 py-4">{{ $book->isbn }}</td>
-                    <td class="px-6 py-4">{{ $book->title }}</td>
-                    <td class="px-6 py-4">{{ $book->author }}</td>
-                    <td class="px-6 py-4">{{ $book->description }}</td>
-                    <td class="px-6 py-4">{{ $book->date_published }}</td>
+                <tr class="bg-red-250 border border-red-200">
+                    <td class="px-6 py-4 border border-red-200">{{ $book->id }}</td>
+                    <td class="px-6 py-4 border border-red-200">{{ $book->isbn }}</td>
+                    <td class="px-6 py-4 border border-red-200">{{ $book->title }}</td>
+                    <td class="px-6 py-4 border border-red-200">{{ $book->author }}</td>
+                    <td class="px-6 py-4 border border-red-200">{{ $book->description }}</td>
+                    <td class="px-6 py-4 border border-red-200">{{ $book->date_published }}</td>
                 </tr>
                 @endforeach
             </tbody>
